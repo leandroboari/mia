bc2f7efe39ec09b6cc461d0128a3cbb4f1771a1d
Leandro Boari <leandrobnsilva@gmail.com>
2018-10-30 16:02:49 -0300

    MIA updates for listen and speak

--- Java/MIA.java
+++ Java/MIA.java
@@ -1,3 +1,14 @@
+import java.util.*;
+
 public class MIA {
 	
+	public void speak(String text) {
+		System.out.println(text);
+	}
+
+	public String listen() {
+		Scanner s = new Scanner(System.in);
+		return s.nextLine();
+	}
+
 }
