6285a1b3a42ffbb5cd20b4fa950dc7ce08a370f5
Leandro Boari <leandrobnsilva@gmail.com>
2018-12-21 11:48:59 -0200

    Adicionado construtor do Interpretador

--- Java/Interpretador.java
+++ Java/Interpretador.java
@@ -1,85 +1,89 @@
 public class Interpretador {
+
 	public static Vector<String> adCompromisso = new Vector<String>();
 	public static Vector<String> verCompromisso = new Vector<String>();
 	public static Vector<String> adNota = new Vector<String>();
 	public static Vector<String> buscaNota = new Vector<String>();
 	public static Vector<String> contaPiada = new Vector<String>();
+
+	public Interpretador() {
+		adCompromisso.add("Colocar compromiso");
+		adCompromisso.add("Inserir compromisso");
+		adCompromisso.add("Adicionar compromiso");
+		adCompromisso.add("Marcar compromiso");
+		adCompromisso.add("Colocar compromiso");
+		adCompromisso.add("Fazer compromiso");
+		adCompromisso.add("Novo compromiso");
+		adCompromisso.add("Colocar um compromiso");
+		adCompromisso.add("Inserir um compromisso");
+		adCompromisso.add("Adicionar um compromiso");
+		adCompromisso.add("Marcar um compromiso");
+		adCompromisso.add("Colocar um compromiso");
+		adCompromisso.add("Fazer um compromiso");
+		adCompromisso.add("Colocar um novo compromiso");
+		adCompromisso.add("Inserir um novo compromisso");
+		adCompromisso.add("Adicionar um novo compromiso");
+		adCompromisso.add("Marcar um novo compromiso");
+		adCompromisso.add("Colocar um novo compromiso");
+		adCompromisso.add("Fazer um novo compromiso");
+		
+		adNota.add("Colocar nota");
+		adNota.add("Inserir nota");
+		adNota.add("Adicionar nota");
+		adNota.add("Marcar nota");
+		adNota.add("Colocar nota");
+		adNota.add("Fazer nota");
+		adNota.add("Nova nota");
+		adNota.add("Colocar uma nota");
+		adNota.add("Inserir uma nota");
+		adNota.add("Adicionar uma nota");
+		adNota.add("Marcar uma nota");
+		adNota.add("Colocar uma nota");
+		adNota.add("Fazer uma nota");
+		adNota.add("Colocar uma nova nota");
+		adNota.add("Inserir uma nova nota");
+		adNota.add("Adicionar uma nova nota");
+		adNota.add("Marcar uma nova nota");
+		adNota.add("Colocar uma nova nota");
+		adNota.add("Fazer uma nova nota");
+
+
+		verCompromisso.add("Mostrar compromisso");
+		verCompromisso.add("Mostrar compromisso");	
+		verCompromisso.add("Ver compromissos");
+		verCompromisso.add("Ver compromisso");
+		verCompromisso.add("Visualizar compromissos");
+		verCompromisso.add("Visualizar compromisso");
+		verCompromisso.add("Mostre meus compromissos");
+		verCompromisso.add("Quais são meus compromissos");
+		verCompromisso.add("Fale meus compromissos");
+		verCompromisso.add("Diga meus compromissos");
+
+		buscaNota.add("Buscar notas");
+		buscaNota.add("Buscar nota");
+		buscaNota.add("Mostrar notas");
+		buscaNota.add("Mostrar nota");
+		buscaNota.add("Procurar notas");
+		buscaNota.add("Procurar nota");
+		buscaNota.add("Achar notas");
+		buscaNota.add("Achar nota");
+		buscaNota.add("Busque notas");
+		buscaNota.add("Busque nota");
+		buscaNota.add("Mostre notas");
+		buscaNota.add("Mostre nota");
+		buscaNota.add("Procure notas");
+		buscaNota.add("Procure nota");
+		buscaNota.add("Ache notas");
+		buscaNota.add("Ache nota");
+		buscaNota.add("Busque uma nota");
+		buscaNota.add("Mostre uma nota");
+		buscaNota.add("Procure uma nota");
+		buscaNota.add("Ache uma nota");
+
+		contaPiada.add();
+	}
+
 	
-
-	adCompromisso.add("Colocar compromiso");
-	adCompromisso.add("Inserir compromisso");
-	adCompromisso.add("Adicionar compromiso");
-	adCompromisso.add("Marcar compromiso");
-	adCompromisso.add("Colocar compromiso");
-	adCompromisso.add("Fazer compromiso");
-	adCompromisso.add("Novo compromiso");
-	adCompromisso.add("Colocar um compromiso");
-	adCompromisso.add("Inserir um compromisso");
-	adCompromisso.add("Adicionar um compromiso");
-	adCompromisso.add("Marcar um compromiso");
-	adCompromisso.add("Colocar um compromiso");
-	adCompromisso.add("Fazer um compromiso");
-	adCompromisso.add("Colocar um novo compromiso");
-	adCompromisso.add("Inserir um novo compromisso");
-	adCompromisso.add("Adicionar um novo compromiso");
-	adCompromisso.add("Marcar um novo compromiso");
-	adCompromisso.add("Colocar um novo compromiso");
-	adCompromisso.add("Fazer um novo compromiso");
-	
-	adNota.add("Colocar nota");
-	adNota.add("Inserir nota");
-	adNota.add("Adicionar nota");
-	adNota.add("Marcar nota");
-	adNota.add("Colocar nota");
-	adNota.add("Fazer nota");
-	adNota.add("Nova nota");
-	adNota.add("Colocar uma nota");
-	adNota.add("Inserir uma nota");
-	adNota.add("Adicionar uma nota");
-	adNota.add("Marcar uma nota");
-	adNota.add("Colocar uma nota");
-	adNota.add("Fazer uma nota");
-	adNota.add("Colocar uma nova nota");
-	adNota.add("Inserir uma nova nota");
-	adNota.add("Adicionar uma nova nota");
-	adNota.add("Marcar uma nova nota");
-	adNota.add("Colocar uma nova nota");
-	adNota.add("Fazer uma nova nota");
-
-
-	verCompromisso.add("Mostrar compromisso");
-	verCompromisso.add("Mostrar compromisso");	
-	verCompromisso.add("Ver compromissos");
-	verCompromisso.add("Ver compromisso");
-	verCompromisso.add("Visualizar compromissos");
-	verCompromisso.add("Visualizar compromisso");
-	verCompromisso.add("Mostre meus compromissos");
-	verCompromisso.add("Quais são meus compromissos");
-	verCompromisso.add("Fale meus compromissos");
-	verCompromisso.add("Diga meus compromissos");
-
-	buscaNota.add("Buscar notas");
-	buscaNota.add("Buscar nota");
-	buscaNota.add("Mostrar notas");
-	buscaNota.add("Mostrar nota");
-	buscaNota.add("Procurar notas");
-	buscaNota.add("Procurar nota");
-	buscaNota.add("Achar notas");
-	buscaNota.add("Achar nota");
-	buscaNota.add("Busque notas");
-	buscaNota.add("Busque nota");
-	buscaNota.add("Mostre notas");
-	buscaNota.add("Mostre nota");
-	buscaNota.add("Procure notas");
-	buscaNota.add("Procure nota");
-	buscaNota.add("Ache notas");
-	buscaNota.add("Ache nota");
-	buscaNota.add("Busque uma nota");
-	buscaNota.add("Mostre uma nota");
-	buscaNota.add("Procure uma nota");
-	buscaNota.add("Ache uma nota");
-
-	contaPiada.add();
 
 	public static String Recebe(String frase) {
 		
